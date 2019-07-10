d7f421c0f455e38d82032938ce3a0a6d60367534
Fengkingka <504275107@qq.com>
2019-07-10 20:58:20 +0800

    vuelearn1

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-
+import Info from './views/Info.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -11,6 +11,11 @@ export default new Router({
       name: 'home',
       component: Home
     },
+		{
+		  path: '/info',
+		  name: 'info',
+		  component: Info
+		},
     {
       path: '/about',
       name: 'about',
---
+++ src/views/Info.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		hello ,info!
+		<button type="button" @click="add()">添加</button>
+	</div>
+</template>
+
+<script>
+	import store from '@/store'
+	export default{
+		name:"Info",
+		methods:{
+			add (){
+				console.log("111111");
+				store.commit('increase')
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
